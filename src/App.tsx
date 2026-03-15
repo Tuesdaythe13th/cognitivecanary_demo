1559ef53e5173110ef1ca5cb84500d013106e11e
tuesdaythe13th <153651490+Tuesdaythe13th@users.noreply.github.com>
2026-03-15 15:10:08 -0700

    merge

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import StandaloneDemo from "./pages/StandaloneDemo";
 
+import Landing from "./pages/Landing";
+
 const queryClient = new QueryClient();
 
 const App = () => (
@@ -16,7 +18,8 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/lab" element={<Index />} />
           <Route path="/demo/:id" element={<StandaloneDemo />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
